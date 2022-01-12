587a3a9c33354633b38933f1632c180fb043532a
Marx J. Moura <marx.jmoura@gmail.com>
2022-01-12 08:26:57 -0300

    Use available screen space

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,22 @@
 $(function () {
 
-  $('.dropdown-menu.keep-open').on('click', function (e) {
+  $('.dropdown-keep-open .dropdown-menu').on('click', function (e) {
     e.stopPropagation();
+  });
+
+  $('.dropdown-detached').on('show.bs.dropdown', function() {
+    var $this = $(this);
+    var $dropdownMenu = $this.find('.dropdown-menu');
+
+    var offset = $this.offset();
+
+    var css = {
+      position: 'absolute',
+      left: offset.left,
+      top: offset.top
+    };
+
+    $('body').append($dropdownMenu.css(css).detach());
   });
 
 });
